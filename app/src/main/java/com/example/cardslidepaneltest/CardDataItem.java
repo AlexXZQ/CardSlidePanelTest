0989349dcfce0cddd47c674a58a18dbbe9063513
XZQ <18024660859@163.com>
2020-08-14 17:20:10 +0800

    完成预期效果

---
+++ app/src/main/java/com/example/cardslidepaneltest/CardDataItem.java
@@ -0,0 +1,13 @@
+package com.example.cardslidepaneltest;
+
+/**
+ * 卡片数据装载对象
+ */
+public class CardDataItem {
+
+    String imagePath;
+    String userName;
+    int likeNum;
+    int imageNum;
+
+}
